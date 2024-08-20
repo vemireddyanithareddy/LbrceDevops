75e6d6478ad6fa16d11604f23f39dc40c83db9d7
AdmiN <AdmiN@ITWS063>
2024-08-20 11:00:14 +0530

    commit the data

--- src/Leapyear.java
+++ src/Leapyear.java
@@ -1,9 +1,9 @@
-      
-public class Welcome {
+
+public class Leapyear {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-    System.out.println("welcome to devops lab");
+
 	}
 
 }
