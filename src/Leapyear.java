fc7603e626c333a8fa3c2eb731f2cde6fa7ab9f9
AdmiN <AdmiN@ITWS063>
2024-08-20 11:00:40 +0530

    commit the data

--- src/Leapyear.java
+++ src/Leapyear.java
@@ -2,7 +2,15 @@
 public class Leapyear {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int a=2003;
+		if(a%4==0 || a%100!=0 && a%400==0)
+		{
+			System.out.println("leap year");
+		}
+		else
+			System.out.println("not a leap year");
+	
+			
 
 	}
 
